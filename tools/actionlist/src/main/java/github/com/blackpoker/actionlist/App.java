567737b1e156a08c27f54a9b3693bdff130b7bbf
iichico <iichico@iichiconoMacBook-Pro.local>
2020-07-24 15:41:15 +0900

    actionlist用のjavaPJを作成

---
+++ tools/actionlist/src/main/java/github/com/blackpoker/actionlist/App.java
@@ -0,0 +1,11 @@
+package github.com.blackpoker.actionlist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
