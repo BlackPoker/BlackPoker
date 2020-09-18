e16b7a691a6054ede67a0bd95bbd9bca2b2e19a9
iichico <iichico@iichiconoMacBook-Pro.local>
2020-09-19 04:31:58 +0900

    v6-stdのpdfが生成できるようになりました。

--- tools/actionlist/build.sh
+++ tools/actionlist/build.sh
@@ -1,3 +1,3 @@
-sh ./tex/tex2pdf.sh blackpoker-v5-std
-python3 ./python/2up-pdf.1.py ./tex/blackpoker-v5-std.pdf
+sh ./tex/tex2pdf.sh blackpoker-v6-std
+python3 ./python/2up-pdf.1.py ./tex/blackpoker-v6-std.pdf
 cp ./tex/*.pdf ./dist
