7a1e36d5e2d205dfb05a25b5116d55c27f8ce7c7
iichico <iichico@iichiconoMacBook-Pro.local>
2020-11-04 07:23:14 +0900

    todoコメント追加

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@ rm -rf ./docs/*
 
 sphinx-build -b html ./source ./docs
 
+# TODO PDFの生成を追加
+
 mkdir -p docs/actionlist/html
 mkdir -p docs/actionlist/pdf
 
 cp -r tools/actionlist/dist/web-site/static/* docs/actionlist/html
+
+# TODO ActionListnoPDFコピーも行う。
