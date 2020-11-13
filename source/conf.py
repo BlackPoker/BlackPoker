d665ec908647097e7de09c2c4f809f515a50caea
iichico <iichico@iichiconoMacBook-Pro.local>
2020-11-14 06:47:57 +0900

    footer定義を追加

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2013, BlackPoker'
 author = 'BlackPoker'
 numfig = True
 # release = '2019/6/30'
-# version = '5th'
+version = '5th'
 
 # -- General configuration ---------------------------------------------------
 
@@ -63,6 +63,26 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    # 'canonical_url': '',
+    # 'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+    'logo_only': False,
+    'display_version': True,
+    # 'prev_next_buttons_location': 'bottom',
+    # 'style_external_links': False,
+    # 'vcs_pageview_mode': '',
+    # 'style_nav_header_background': 'black',
+    # Toc options
+    # 'collapse_navigation': True,
+    # 'sticky_navigation': True,
+    # 'navigation_depth': 4,
+    # 'includehidden': True,
+    # 'titles_only': False
+}
+
+html_show_sourcelink = True
+
 
 [extensions]
 todo_include_todos=True
