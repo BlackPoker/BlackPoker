227044f90a5aa86c6f121222fd450bf168ca641e
iichico <iichico@iichiconoMacBook-Pro.local>
2021-01-10 11:01:23 +0900

    空にしました。

--- tools/actionlist/src/main/java/com/github/blackpoker/actionlist/Writer.java
+++
@@ -1,8 +0,0 @@
-package com.github.blackpoker.actionlist;
-
-import java.io.IOException;
-import java.util.Map;
-
-public interface Writer {
-	void write(Map<String, Object> map, String outPath,String templateName) throws IOException ;
-}
