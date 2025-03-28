cd0215b6a9c64c1ea051e4e0f7033edbe9cfda47
BlackPoker <blackpoker.official@gmail.com>
2025-03-28 23:34:16 +0000

    refresh docs

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2025/3/x',
+    VERSION: '2025/3/31',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
