540a3711e4cd61bfb78f27da9d1522efbb29a65e
BlackPoker <blackpoker.official@gmail.com>
2026-07-20 13:44:36 +0000

    refresh docs

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2026/06/30',
+    VERSION: '2026/07/31',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
